6405393514b5c50a7e5e2ec4c76759117412fcb6
Jherrera <jherrera12@gmail.com>
2024-12-12 08:20:11 -0500

    carga pn

--- content/db/querys.py
+++ content/db/querys.py
@@ -14,3 +14,12 @@ def read_dictionary_campain():
         from		sinfin.dic_naturgy
     """
     return text
+
+def query_nomina():
+    text = """
+        select		sucursal, count(sucursal)as q_gestores
+        from		data_sinfin.nomina
+        where		"ID_CARTERA" = 1 AND CARGO = 'Gestor' AND ESTADO_GESTOR = 'Activo'
+        group by	sucursal
+    """
+    return text
